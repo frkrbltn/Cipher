653a5b88ad4c32204876e2e21abab90a1173c613
Furkan Karabulut <fkarabu@ncsu.edu>
2021-11-26 05:12:11 -0500

    Create the arguments

--- Cipher/src/Cipher.java
+++ Cipher/src/Cipher.java
@@ -28,77 +28,77 @@ public class Cipher {
             System.out.print("Cannot create output file.");
             System.exit(1);
         }
+    }
 
-        /**
-         * This method returns true if the cipher is not null and
-         * contains exactly 26 letters
-         * one for each letter of alphabet
-         * Otherwise returns false.
-         * @param cipher is char array
-         * @return true cipher is not null and contains 26 letters otherwise
-         * is false.
-         */
-        public static boolean isValidCipher(char[] cipher) {
-            if(cipher.length != 26) {
-                return false;
-            } else {
-                for (int i = 0; i < cipher.length; i++) {
-                    if (cipher[i] == 0) {
-                        return false;
-                    } else {
-                        return true;
-                    }
+     /**
+      * This method returns true if the cipher is not null and
+      * contains exactly 26 letters
+      * one for each letter of alphabet
+      * Otherwise returns false.
+      * @param cipher is char array
+      * @return true cipher is not null and contains 26 letters otherwise
+      * is false.
+      */
+    public static boolean isValidCipher (char[] cipher) {
+        if(cipher.length != 26) {
+            return false;
+        } else {
+            for (int i = 0; i < cipher.length; i++) {
+                if (cipher[i] == 0) {
+                    return false;
+                } else {
+                    return true;
                 }
             }
         }
+    }
 
-        /**
-         * This method process the work of the main idea
-         * if encrypt is true, encrypts the lines in the input file
-         * using the cipher and outputs them to the output file
-         *
-         * if encrypt is false, decrypts the lines in the input file
-         * using the cipher and outputs them to the output file
-         *
-         * @param encrypt the boolean value will tell if encrypts or decrypts
-         * @param in will scan the input file
-         * @param out will out into the output file
-         * @param cipher is the char array
-         * @throws IllegalArgumentException with message Invalid Parameter, if in is null,
-         * out is null and cipher is invalid
-         */
-        public static void processFile(boolean encrypt, Scanner in, PrintWriter out, char[] cipher) {
+    /**
+     * This method process the work of the main idea
+     * if encrypt is true, encrypts the lines in the input file
+     * using the cipher and outputs them to the output file
+     *
+     * if encrypt is false, decrypts the lines in the input file
+     * using the cipher and outputs them to the output file
+     *
+     * @param encrypt the boolean value will tell if encrypts or decrypts
+     * @param in will scan the input file
+     * @param out will out into the output file
+     * @param cipher is the char array
+     * @throws IllegalArgumentException with message Invalid Parameter, if in is null,
+     * out is null and cipher is invalid
+     */
+    public static void processFile(boolean encrypt, Scanner in, PrintWriter out, char[] cipher) {
 
+    }
+
+    /**
+     * Return string containing encrypted line
+     * @param line process will be working on the given line
+     * @param cipher is the char array
+     * @return string line which encrypted line
+     * @throws IllegalArgumentException with message Invalid Parameter, if line is null
+     * or cipher will be invalid
+     */
+    public static String encryptLine(String line, char[] cipher) {
+        String s = "";
+        for (int i = 0; i < line.length(); i++) {
+            s += line.charAt(i);
         }
+        return s;
+    }
 
-        /**
-         * Return string containing encrypted line
-         * @param line process will be working on the given line
-         * @param cipher is the char array
-         * @return string line which encrypted line
-         * @throws IllegalArgumentException with message Invalid Parameter, if line is null
-         * or cipher will be invalid
-         */
-        public static String encryptLine(String line, char[] cipher) {
-            String s = "";
-            for (int i = 0; i < line.length(); i++) {
-                s += line.charAt(i);
-            }
-            return s;
+    /**
+     * Return string containing decrypted line
+     * @param line process will be working on the given line
+     * @param cipher is the char array
+     * @return string line whihc the decrypted line
+     */
+    public static String decryptLine(String line, char[] cipher) {
+        String s = "";
+        for (int i = 0; i < line.length(); i++) {
+            s += line.charAt(i);
         }
-
-        /**
-         * Return string containing decrypted line
-         * @param line process will be working on the given line
-         * @param cipher is the char array
-         * @return string line whihc the decrypted line
-         */
-        public static String decryptLine(String line, char[] cipher) {
-            String s = "";
-            for (int i = 0; i < line.length(); i++) {
-                s += line.charAt(i);
-            }
-            return s;
-        }
+        return s;
     }
 }
